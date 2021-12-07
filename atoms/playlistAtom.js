03f1fb0c24c3de974fc705dff974bdc28530c894
Bishal-9 <goonbishal.bg@gmail.com>
2021-12-07 22:29:29 +0530

    Center component

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -4,3 +4,8 @@ export const playlistIdState = atom({
     key: 'playlistIdState',
     default: '37i9dQZF1DX94qaYRnkufr'
 })
+
+export const playlistState = atom({
+    key: 'playlistState',
+    default: null
+})
--- components/Center.jsx
+++ components/Center.jsx
@@ -2,8 +2,10 @@ import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import { shuffle } from 'lodash'
 import { ChevronDownIcon } from '@heroicons/react/outline'
-import { useRecoilValue } from "recoil"
-import { playlistIdState } from "../atoms/playlistAtom"
+import { useRecoilState, useRecoilValue } from "recoil"
+import { playlistState, playlistIdState } from "../atoms/playlistAtom"
+import useSpotify from '../hooks/useSpotify'
+import Songs from "./Songs"
 
 const colors = [
     'from-indigo-500',
@@ -19,16 +21,27 @@ function Center() {
 
     const { data: session } = useSession()
     const [color, setColor] = useState(null)
+    const spotifyApi = useSpotify()
     const playlistId = useRecoilValue(playlistIdState)
+    const [playlist, setPlaylist] = useRecoilState(playlistState)
 
     useEffect(() => {
         setColor(shuffle(colors).pop())
     }, [playlistId])
 
+    useEffect(() => {
+        spotifyApi
+            .getPlaylist(playlistId)
+            .then(data => setPlaylist(data.body))
+            .catch(error => `Something went wrong!! ${error.response}`)
+    }, [spotifyApi, playlistId])
+
+    console.log(playlist) 
+
     return (
-        <div className='flex-grow'>
+        <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide'>
             <header className='absolute top-5 right-8'>
-                <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+                <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white'>
                     <img
                         className='rounded-full w-10 h-10'
                         src={session?.user?.image}
@@ -39,8 +52,20 @@ function Center() {
                 </div>
             </header>
             <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
-                <img src="" alt="" />
+                <img
+                    className='h-44 w-44 shadow-2xl'
+                    src={playlist?.images?.[0]?.url}
+                    alt=""
+                />
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>{playlist?.name}</h1>
+                </div>
             </section>
+
+            <div>
+                <Songs />
+            </div>
         </div>
     )
 }
